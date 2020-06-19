35e09fa471d235e3fef8fafb43a25ece1c56e121
zjj <1621755657@qq.com>
2020-06-19 16:14:18 +0800

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    API
+                    APP
                 </div>
 
                 <div class="links">
