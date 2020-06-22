27a9fb24df9270c4006c1cd1fceb838f416e3b9e
zjj <1621755657@qq.com>
2020-06-22 14:54:09 +0800

    redis测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redis;
 class TestController extends Controller
 {
-    //
+    //redis测试
+    public function redis1(){
+        $key = 'name1';
+        $val1 = Redis::get($key);
+        var_dump($val1);echo'</br>';
+        echo '$val1:'.$val1;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('/test/hello',"TestController@hello");
+Route::get('/test/redis1',"TestController@redis1");
 
-Route::get('/goods/detail',"Goods\GoodsController@detail");
+
+Route::get('/goods/detail',"Goods\GoodsController@detail");//商品详情
+
+
